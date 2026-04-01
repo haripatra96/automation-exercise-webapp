bd149d712e429cda271682d701a7c3f01cb65f12
Hari Prasad <patra.hari1411@gmail.com>
2026-04-01 07:48:56 +0530

    Rebasing

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -8,6 +8,6 @@ public final class FrameworkConstants {
 	public final static String CONFIG_Path=USER_DIR+"/src/test/resource/config.properties";
 	public final static String REPORT_PATH= "";
     public static final int EXPLICIT_WAIT = 10;
-    public static final String NEW_REPORT_PATH = "";
+//    public static final String NEW_REPORT_PATH = "";
 
 }
